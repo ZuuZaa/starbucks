a32e6fd35c5f8003625c4ebe3b8e7fd670bc2fbd
ZuuZaa <zumbaghirova@gmail.com>
2023-04-20 14:36:16 +0400

    slider card shadows

--- src/features/gifts/slider /index.jsx
+++ src/features/gifts/slider /index.jsx
@@ -1,21 +1,52 @@
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
-export const Slider = ({data}) => {
+export const Slider = ({ data }) => {
   return (
     <div className='slider-container'>
-      <div className="handle left-handle"></div>
-      <div className="slider">
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
-        <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+      <div className="handle left-handle">
+        <button>
+          <BsChevronLeft />
+        </button>
       </div>
-      <div className="handle right-handle"></div>
+      <div className="slider">
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+        <Link to="/">
+          <img src="https://placehold.co/600x400/EEE/31343C" alt="img" />
+        </Link>
+      </div>
+      <div className="handle right-handle">
+        <button>
+          <BsChevronRight />
+        </button>
+      </div>
     </div>
   )
 }
