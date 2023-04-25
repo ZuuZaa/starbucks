21d75c2a8fb03dad3cce01beac28a4fd7173c52c
ZuuZaa <zumbaghirova@gmail.com>
2023-04-25 11:47:03 +0400

    slider

--- src/features/gifts/slider /index.jsx
+++ src/features/gifts/slider /index.jsx
@@ -1,44 +1,61 @@
 import { nanoid } from 'nanoid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import { SimpleLink } from 'styled/buttons';
 import './style.scss';
 
-export const Slider = ({ data }) => {
 
+export const Slider = ({ data, title }) => {
 
   const [sliderIndex, setSliderIndex] = useState(0)
-
-  const sliderSize = 4 * sliderIndex;
-
-
+  const [size, setSize] = useState(4);
+  const sliderSize = size * sliderIndex;
 
   const prev = () => sliderSize > 0 && setSliderIndex(sliderIndex - 1);
-  const next = () =>  sliderSize < data.length - 4 &&  setSliderIndex(sliderIndex + 1); console.log("click")
+  const next = () => sliderSize < data.length - size && setSliderIndex(sliderIndex + 1); console.log("click")
 
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      const screen = window.innerWidth
+      screen > 1024 ? setSize(4): screen < 1024 && screen > 768 ? setSize(3) : setSize(2)
+      console.log(size)
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+  }, [size]);
 
 
   return (
+    <div className='slider-main'>
+    <div className="slider-header">
+    <h2>{title}</h2>
+  <SimpleLink>see all</SimpleLink>
+  </div>
     <div className='slider-container'>
-      <div className="handle left-handle"> 
+
+      <div className="handle handle--left">
         <button onClick={prev} className={sliderIndex > 0 ? 'visible-button' : 'hidden-button'}>
-          <BsChevronLeft  />
+          <BsChevronLeft />
         </button>
       </div>
       <div className="slider" style={{ transform: `translateX(calc(${sliderIndex} * -100%))` }}>
         {
-          data.map(url=>
-            <Link to="/" key={nanoid()}>
-              <img src={url} alt="img" />
+          data.map(url =>
+            <Link to="/"  key={nanoid()}>
+              <img src={url} alt="gift card" />
             </Link>
           )
         }
       </div>
-      <div className="handle right-handle">
-        <button onClick={next} className={ data.length / 4 > 1 ? 'visible-button' : 'hidden-button'}>
-          <BsChevronRight/>
+      <div className="handle handle--right">
+        <button onClick={next} className={data.length / size > 1 ? 'visible-button' : 'hidden-button'}>
+          <BsChevronRight />
         </button>
       </div>
+    </div>
     </div>
   )
 }
--- src/pages/Gifts.jsx
+++ src/pages/Gifts.jsx
@@ -4,7 +4,7 @@ import { giftsData } from 'utils/gifts';
 export const Gifts = () => {
   return (
     <main className='page gift-page'>
-      <Slider data={giftsData[0].cards} />
+      <Slider data={giftsData[0].cards} title='Featured' />
     </main>
   )
 }
