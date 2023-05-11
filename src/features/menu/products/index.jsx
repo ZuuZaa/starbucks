14e416945a7033e2659cc6059ee436185efeaead
ZuuZaa <zumbaghirova@gmail.com>
2023-05-11 16:36:31 +0400

    menu items

--- src/features/menu/products/index.jsx
+++ src/features/menu/products/index.jsx
@@ -1,7 +1,33 @@
 import './style.scss';
+import data from "data/MenuAllDatas";
 
 export const Products = () => {
+  console.log(data)
   return (
-    <div>Products</div>
+    <div className='all-products'>
+      <h1 className='section-header'>Menu</h1>
+      <ul>
+        {
+          data.allCategory.map(
+            item =>
+              <li>
+                <h2 className='category-name'>{item.category}</h2>
+                <ul className="products-list">
+                  {item.products.map(
+                    product =>
+                      <li>
+                        <div className="img-wrapper">
+                          <img src={product.img} alt="coffee" />
+                        </div>
+                        <p>{product.name}</p>
+                      </li>
+                  )}
+                </ul>
+              </li>
+          )
+        }
+      </ul>
+
+    </div>
   )
 }
